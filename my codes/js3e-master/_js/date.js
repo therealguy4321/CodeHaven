a8595ecaf5392cbe7f222a9cd06691ef043cb8a6
Ogbebo Christopher <cogbebo@gmail.com>
2025-03-06 18:31:35 +0100

    Removed unwanted files from repository

--- my codes/js3e-master/_js/date.js
+++
@@ -1,4 +0,0 @@
-function printToday() {
-  var today = new Date();
-  document.write(today.toDateString());
-}
--- my codes/js3e-master/_js/time.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	function displayTime() {
-		$('#time').text(getTime(true));
-	}
-	displayTime();
-	setInterval(displayTime,1000);
-}); // end ready
